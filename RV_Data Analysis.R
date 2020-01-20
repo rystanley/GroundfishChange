60d5bb52888d7bd11edb9bf596119e657aeda420
aczich <aczich@users.noreply.github.com>
2020-01-20 12:16:28 -0400

    added ecdfs by season

--- RV_Data Analysis.R
+++ RV_Data Analysis.R
@@ -6,7 +6,7 @@ library(ggplot2) #plotting
 library(data.table) #faster processing
 
 
-rv = fread("Halibut_2018_RV.csv")
+rv = fread("C:/Users/User/Documents/School 2019-2020/Halibut/Halibut_2018_RV.csv")
 
 #Select columns that will be used
 rv = rv %>% select(LATITUDE, LONGITUDE, YEAR, SEASON, NAME, DMAX, BOTTOM_TEMPERATURE, BOTTOM_SALINITY, TOTALNUMBERSTANDARDIZED, TOTALWEIGHTSTANDARDIZED_KG, PRESENCE) %>% rename(Zone = NAME, Depth = DMAX, Temp = BOTTOM_TEMPERATURE, Year = YEAR, Season = SEASON, Salinity = BOTTOM_SALINITY, Abundance = TOTALNUMBERSTANDARDIZED, Kg = TOTALWEIGHTSTANDARDIZED_KG, Presence = PRESENCE)
@@ -25,16 +25,15 @@ obs_hal = rv_temp %>% filter(Presence == "P") %>% uncount(Abundance)
 
 
   
-# Plots all areas sampled by temp
+## Plots all areas sampled by temp ----
 ggplot(rv_temp, aes(x = T_range)) + geom_histogram(stat = "count") +
   theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) + theme(axis.line = element_line(colour = "black"),
                                                                       panel.grid.major = element_blank(),
                                                                       panel.grid.minor = element_blank(),
                                                                       panel.background = element_blank()) +
   ggtitle("Temperatures Surveyed")#outer = -1-13 , middle = 1-10 , inner = 4-8
-#Not Binned barlot
-ggplot(obs_hal, aes(x = Temp, y = Presence)) + geom_bar(stat = "identity")  
-## Binned temperature plot (does'nt tell how where the survey mostly sampled)
+  
+## Binned temperature plot
 ggplot(obs_hal, aes(x = T_range)) + geom_histogram(stat = "count") +
   theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) + theme(axis.line = element_line(colour = "black"),
                                                                       panel.grid.major = element_blank(),
@@ -60,8 +59,7 @@ ggplot(rv_depth, aes(x = D_range)) + geom_histogram(stat = "count") +
                                                                       panel.grid.minor = element_blank(),
                                                                       panel.background = element_blank()) +
   ggtitle("Depths Surveyed") #outer = 11-150 , middle = 20-91 , inner = 31-61
-#Not Binned barlot
-ggplot(obs_depth_hal, aes(x = Depth, y = Presence)) + geom_bar(stat = "identity")  
+ 
 ## Binned temperature plot (does'nt tell how where the survey mostly sampled)
 
 ggplot(obs_depth_hal, aes(x = D_range)) + geom_histogram(stat = "count") +
@@ -72,33 +70,68 @@ ggplot(obs_depth_hal, aes(x = D_range)) + geom_histogram(stat = "count") +
   ggtitle("Abundance of Halibut by Depth") # outer = 20-170 , middle = 30-120, inner 30-80, narrow inner = 40-60
 #create binned barplot for all areas sampled present or not
 
-
-
-
-
-## ECDF Temp 
+## ECDF plots ----
+ 
 # Survey area
-sT_ecdf = rv %>% filter(!is.na(Temp))
-
+surv_ecdf = rv %>% filter(!is.na(Temp))
 #Abundance
 abun_ecdf = rv %>% filter(Presence == "P", !is.na(Temp)) %>% uncount(Abundance)
-  
+
+
+## ECDF Temp  
 ggplot() + stat_ecdf(data = abun_ecdf, mapping = aes(Temp, colour = Presence), geom = "step", pad = FALSE) + 
   stat_ecdf(data = sT_ecdf, mapping = aes(Temp), geom = "step", pad = FALSE) + 
-  scale_x_continuous(breaks = seq(-2, 17, by = 1)) + theme(axis.line = element_line(colour = "black"),
+  scale_x_continuous(breaks = seq(-2, 17, by = 1), limits = c(-2,15)) + theme(axis.line = element_line(colour = "black"),
                                                            panel.grid.major = element_blank(),
                                                            panel.grid.minor = element_blank(),
                                                            panel.background = element_blank()) +
   ggtitle("Temperature Surveyed vs Presence of Halibut")
 
-# ECDF Temp
-sD_ecdf = rv %>% filter(!is.na(Temp))
-abun_dep_ecdf = rv %>% filter(Presence == "P", !is.na(Temp)) %>% uncount(Abundance)
+## ECDF Depth 
 
-ggplot() + stat_ecdf(data = abun_dep_ecdf, mapping = aes(Depth, colour = Presence), geom = "step", pad = FALSE) + 
-  stat_ecdf(data = sD_ecdf, mapping = aes(Depth), geom = "step", pad = FALSE) + 
-  scale_x_continuous(breaks = seq(0, 1000, by = 50)) + theme(axis.line = element_line(colour = "black"),
+ggplot() + stat_ecdf(data = abun_ecdf, mapping = aes(Depth, colour = Presence), geom = "step", pad = FALSE) + 
+  stat_ecdf(data = surv_ecdf, mapping = aes(Depth), geom = "step", pad = FALSE) + 
+  scale_x_continuous(breaks = seq(0, 1000, by = 50), limits = c(0,400)) + theme(axis.line = element_line(colour = "black"),
                                                              panel.grid.major = element_blank(),
                                                              panel.grid.minor = element_blank(),
                                                              panel.background = element_blank()) +
   ggtitle("Depth Surveyed vs Presence of Halibut")
+
+## ECDF Salinity 
+ggplot() + stat_ecdf(data = abun_ecdf, mapping = aes(Salinity, colour = Presence), geom = "step", pad = FALSE) + 
+  stat_ecdf(data = surv_ecdf, mapping = aes(Salinity), geom = "step", pad = FALSE) + 
+  scale_x_continuous(breaks = seq(25, 37, by = 1), limits = c(29,37)) + theme(axis.line = element_line(colour = "black"),
+                                                             panel.grid.major = element_blank(),
+                                                             panel.grid.minor = element_blank(),
+                                                             panel.background = element_blank()) +
+  ggtitle("Salinity Surveyed vs Presence of Halibut")
+
+## ECDF Plots by Season ----
+
+## ECDF Temp  
+ggplot() + stat_ecdf(data = abun_ecdf, mapping = aes(Temp, colour = Presence), geom = "step", pad = FALSE) + 
+  stat_ecdf(data = sT_ecdf, mapping = aes(Temp), geom = "step", pad = FALSE) + facet_wrap(~Season) +
+  scale_x_continuous(breaks = seq(-2, 17, by = 1), limits = c(-2,15)) + theme(axis.line = element_line(colour = "black"),
+                                                                              panel.grid.major = element_blank(),
+                                                                              panel.grid.minor = element_blank(),
+                                                                              panel.background = element_blank()) +
+  ggtitle("Temperature Surveyed vs Presence of Halibut")
+
+## ECDF Depth 
+
+ggplot() + stat_ecdf(data = abun_ecdf, mapping = aes(Depth, colour = Presence), geom = "step", pad = FALSE) + 
+  stat_ecdf(data = surv_ecdf, mapping = aes(Depth), geom = "step", pad = FALSE) + facet_wrap(~Season) +
+  scale_x_continuous(breaks = seq(0, 1000, by = 50), limits = c(0,400)) + theme(axis.line = element_line(colour = "black"),
+                                                                                panel.grid.major = element_blank(),
+                                                                                panel.grid.minor = element_blank(),
+                                                                                panel.background = element_blank()) +
+  ggtitle("Depth Surveyed vs Presence of Halibut")
+
+## ECDF Salinity 
+ggplot() + stat_ecdf(data = abun_ecdf, mapping = aes(Salinity, colour = Presence), geom = "step", pad = FALSE) + 
+  stat_ecdf(data = surv_ecdf, mapping = aes(Salinity), geom = "step", pad = FALSE) + facet_wrap(~Season) +
+  scale_x_continuous(breaks = seq(25, 37, by = 1), limits = c(29,37)) + theme(axis.line = element_line(colour = "black"),
+                                                                              panel.grid.major = element_blank(),
+                                                                              panel.grid.minor = element_blank(),
+                                                                              panel.background = element_blank()) +
+  ggtitle("Salinity Surveyed vs Presence of Halibut")
