0ed4747013b116dd14bbdbb923d644027e34e5f7
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2021-06-14 22:05:41 -0300

    Updated and fixed with the full extent data, though I don't know what is going on with the mutate error

--- R/depth_fill.R
+++ R/depth_fill.R
@@ -7,35 +7,38 @@ library(raster)
 latlong <- "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
 
 #Load in the Maritimes Data
-load("data/RVData_Summarized_Feb_2021.RData")
+#load("data/RVData_Summarized_Feb_2021.RData")
+load("data/RV_PAdata.RData")
+
+RVdata <- rv_pata
 
 #Load in the DEM that is a pretty good resolution
-load("data/PredSpaceData.RData")
-bathydat <- predSpace%>%dplyr::select("plon","plat","z")
-colnames(bathydat) <- c("long","lat","depth")
-coordinates(bathydat) <- ~long+lat
-proj4string(bathydat) <- "+proj=utm +zone=20 +datum=NAD83 +units=km +no_defs +ellps=GRS80 +towgs84=0,0,0"
-gridded(bathydat) <- TRUE
-dem <- raster(bathydat)
+# load("data/PredSpaceData.RData")
+# bathydat <- predSpace%>%dplyr::select("plon","plat","z")
+# colnames(bathydat) <- c("long","lat","depth")
+# coordinates(bathydat) <- ~long+lat
+# proj4string(bathydat) <- "+proj=utm +zone=20 +datum=NAD83 +units=km +no_defs +ellps=GRS80 +towgs84=0,0,0"
+# gridded(bathydat) <- TRUE
+# dem <- raster(bathydat)
 
-gebco <- raster("data/gebco_2019_Canada.tif")
+gebco <- raster("data/gebco_halibut_extent.tif")
 
 #fill in the coordinate 'holes'
 rv <- RVdata%>%
-      filter(!is.na(LATITUDE))%>% #filter null coordinates
-      st_as_sf(coords=c("LONGITUDE","LATITUDE"),crs=latlong,remove=FALSE)%>%
-      st_transform(proj4string(dem))%>%
-      mutate(depth = extract(dem,as_Spatial(.)))%>%
-      st_transform(proj4string(gebco))%>%
-      mutate(depth2 = extract(gebco,as_Spatial(.)),
-             depth_integrated = ifelse(is.na(DEPTH),ifelse(is.na(depth),depth2,depth),DEPTH))%>% #integrate. Take the value of depth from the RV first, then the high resolution, then the lowest resolution in GEBCO
-      st_transform(latlong)
-      
+      filter(!is.na(latitude))%>% #filter null coordinates
+      st_as_sf(coords=c("longitude","latitude"),crs=latlong,remove=FALSE)%>%
+      st_transform(proj4string(gebco))
 
-sum(is.na(rv$DEPTH))
-sum(is.na(rv$depth))
-sum(is.na(rv$depth2))
-sum(is.na(rv$depth_integrated))
+##there was an error with mutate that I can't really understand 
+rv$depth_gebco <- extract(gebco,as_Spatial(rv))
+
+rv <- rv%>%mutate(depth_integrated = ifelse(is.na(depth),depth_gebco,depth))%>%
+      st_transform(latlong)
+
+#checks on the output
+sum(is.na(rv$depth)) #the number of missing points
+sum(is.na(rv$depth_gebco)) #should be zero
+sum(rv$depth_integrated == rv$depth_gebco) #should be the same as the number of missing points
 
 #save the output
 write.csv(data.frame(rv)%>%dplyr::select(-geometry),"data/MaritimesDepthFilled.csv",row.names = F)
